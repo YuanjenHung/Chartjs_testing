219b7322f913dd778d7046f49ba9334e982739ca
YuanjenHung <a0986859219@gmail.com>
2021-11-26 16:49:24 +0100

    first sketch

--- HAR_data_testing/js/script.js
+++ HAR_data_testing/js/script.js
@@ -1,16 +1,100 @@
-/* 
-const times = [];
-const temperatures = [];
-
-const {InfluxDB} = require('@influxdata/influxdb-client')
-
-// You can generate an API token from the "API Tokens Tab" in the UI
+// import latest release from npm repository 
+import {InfluxDB, Point} from 'https://unpkg.com/@influxdata/influxdb-client/dist/index.browser.mjs';
+      
+const url = 'https://eu-central-1-1.aws.cloud2.influxdata.com/'
 const token = 'Smrm037Xfq6s4RzcMRkik81q9S6zO8LY8u6mDxPUyOoFmvlrz9C12P63a2eF_xrrrPR8YoIbEBmavRM1yszdaw=='
 const org = 'yuanjen.hung@student.supsi.ch'
 const bucket = 'HAR_system'
 
-// Define client
-const client = new InfluxDB({url: 'https://eu-central-1-1.aws.cloud2.influxdata.com', token: token})
+const client = new InfluxDB({ url, token });
+
+const queryApi = client.getQueryApi(org);
+
+function configString(label, color, unit){
+    const config = {
+        type: 'line',
+      
+        data: {
+            labels: [],
+            datasets: [{
+                label: label,
+                backgroundColor: color,
+                borderColor: color,
+                data: [],
+                pointHitRadius: 50
+            }]
+        },
+      
+        options: { 
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    }
+                },
+                y: {
+                    grid: {
+                        display: false,
+                    }
+                }
+            },
+            plugins: { 
+                tooltip: { 
+                callbacks: { 
+                    label: function(context) {
+                        return context.parsed.y + ` ${unit}`;
+                    }
+                } 
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            },
+            elements: {
+                point:{
+                    radius: 0
+                }
+            }
+        }
+    };
+    return config;
+}
+
+function fillUpData(range, measurement, host, aggregateWindow, chart, id, unit){
+    const query = `from(bucket: "HAR_system") |> range(start: ${range}) |> filter(fn: (r) => r["_measurement"] == "${measurement}") |> filter(fn: (r) => r["_field"] == "value") |> filter(fn: (r) => r["host"] == "${host}") |> aggregateWindow(every: ${aggregateWindow}, fn: mean, createEmpty: false)`;
+    
+    var latestData = 0;
+
+    queryApi.queryRows(query, {
+        next(row, tableMeta) {
+            const o = tableMeta.toObject(row);
+            const timeFormat = o._time.slice(o._time.indexOf("T")+1, o._time.indexOf("Z"));
+            const timeBuffer = timeFormat.split(":");
+            const hour = timeBuffer[0];
+            const minute = timeBuffer[1];
+            const second = timeBuffer[2];
+            console.log(`${timeFormat}\t${o._measurement}(${o._field}) = ${o._value.toFixed(1)}`);
+            addData(chart, `${timeBuffer[0]}:${timeBuffer[1]}`, o._value.toFixed(1));
+            latestData = o._value.toFixed(1);
+        },
+        error(error) {
+            console.error(error);
+            console.log('Finished ERROR');
+        },
+        complete() {
+            console.log('Finished SUCCESS');
+            console.log(latestData);
+            if (id == "bulb_bathroom") {
+                if (latestData > 50) {
+                    document.getElementById("bulb_bathroom").classList.add("text-warning");
+                    document.getElementById("bulb_desc_bathroom").innerHTML = "Inside the Bathroom is bright and shine";
+                }
+            } else {
+                document.getElementById(id).innerHTML = latestData + unit;
+            }
+        },
+    })
+}
 
 function addData(chart, label, data) {
     chart.data.labels.push(label);
@@ -20,106 +104,24 @@ function addData(chart, label, data) {
     chart.update();
 }
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
+const bathTempChart = new Chart(
+  document.getElementById('bathTempChart'),
+  configString("Temperature at the past 24h", "rgb(255, 99, 132)", "°C")
+);
 
-const updateLabels = [
-    'Jaly',
-    'Augest',
-    'September',
-    'October',
-    'November',
-    'December',
-];
+fillUpData("-1d", "temperature", "arduino_bathroom", "30m", bathTempChart, "temp_bathroom", " °C");
 
-saleData = [0, 10, 5, 2, 20, 30, 45];
-updateSaleData =  [40, 20, 15, 12, 25, 32, 40];
+const bathHumiChart = new Chart(
+    document.getElementById('bathHumiChart'),
+    configString("Humidity at the past 24h", "rgb(54, 162, 235)", "%")
+);
+  
+fillUpData("-1d", "humidity", "arduino_bathroom", "30m", bathHumiChart, "humi_bathroom", " %");
 
-const data = {
-    labels: [],
-    datasets: [{
-        label: 'Total sale',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [],
-    }]
-};
+const bathLightChart = new Chart(
+    document.getElementById('bathLightChart'),
+    configString("Luminance at the past 24h", "rgb(255, 205, 86)", "Lux")
+);
+  
+fillUpData("-1d", "light", "arduino_bathroom", "30m", bathLightChart, "bulb_bathroom", " Lux");
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-
-// const myChart = new Chart(
-//     document.getElementById('myChart'),
-//     config
-// );
-
-// for (i = 0; i < updateLabels.length; i++) {
-//     addData(myChart, updateLabels[i], updateSaleData[i]);
-// }
-
-// Query time
-const queryApi = client.getQueryApi(org)
-
-const query = `from(bucket: "HAR_system") |> range(start: -1h) |> filter(fn: (r) => r["_measurement"] == "temperature") |> filter(fn: (r) => r["_field"] == "value") |> filter(fn: (r) => r["host"] == "arduino_bathroom")`
-queryApi.queryRows(query, {
-    next(row, tableMeta) {
-        const o = tableMeta.toObject(row)
-        times.push(o._time);
-        temperatures.push(o._value);
-        console.log(`${o._time}\t${o._measurement}(${o._field}) = ${o._value}`)
-    },
-    error(error) {
-        console.error(error)
-        console.log('Finished ERROR')
-    },
-    complete() {
-        console.log('Finished SUCCESS');
-        // console.log(labels);
-        // console.log(labels.length);
-        // console.log(temperatures);
-        // console.log(temperatures.length);
-    },
-})
-*/
-
-import {InfluxDB, Point} from 'https://unpkg.com/@influxdata/influxdb-client/dist/index.browser.mjs'
-
-// You can generate an API token from the "API Tokens Tab" in the UI
-const token = 'Smrm037Xfq6s4RzcMRkik81q9S6zO8LY8u6mDxPUyOoFmvlrz9C12P63a2eF_xrrrPR8YoIbEBmavRM1yszdaw=='
-const org = 'yuanjen.hung@student.supsi.ch'
-const bucket = 'HAR_system'
-
-// Define client
-const client = new InfluxDB({url: 'https://eu-central-1-1.aws.cloud2.influxdata.com', token: token})
-
-const queryApi = client.getQueryApi(org)
-
-const query = `from(bucket: "HAR_system") |> range(start: -1h) |> filter(fn: (r) => r["_measurement"] == "temperature") |> filter(fn: (r) => r["_field"] == "value") |> filter(fn: (r) => r["host"] == "arduino_bathroom")`
-queryApi.queryRows(query, {
-    next(row, tableMeta) {
-        const o = tableMeta.toObject(row)
-        // times.push(o._time);
-        // temperatures.push(o._value);
-        console.log(`${o._time}\t${o._measurement}(${o._field}) = ${o._value}`)
-    },
-    error(error) {
-        console.error(error)
-        console.log('Finished ERROR')
-    },
-    complete() {
-        console.log('Finished SUCCESS');
-        // console.log(labels);
-        // console.log(labels.length);
-        // console.log(temperatures);
-        // console.log(temperatures.length);
-    },
-})
